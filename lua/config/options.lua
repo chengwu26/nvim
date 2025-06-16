7319ce8914aaf7f3fe79763867c382b82ee41e04
kanggui <kg.he@outlook.com>
2025-06-16 20:44:18 +0800

    feat: use clip.exe and Powershell instead win32yank
    The Arch Linux root user (only root user, in my environment)
    invoke `win32yank.exe` will get an error: Invalid Arguments

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -136,66 +136,20 @@ vim.diagnostic.config({
 })
 
 -- [[ WSL clipboard integrate ]]
--- This solution use 'win32yank' to integrate system clipboard.
---  If not install win32yank, the code below will install it into `win32yank_dir`.
--- NOTE: (DEPENDENCE)
---  curl, unzip: install win32yank if it not installed (just for WSL)
-
 if os.getenv("WSL_DISTRO_NAME") ~= nil then
-  -- build win32yank's install directory
-  local env_path = vim.env.PATH
-  local win32yank_dir = vim.fn.stdpath("data") .. "/win32yank"
-  vim.env.PATH = win32yank_dir .. ":" .. env_path
-
-  if vim.fn.executable("win32yank.exe") == 0 then
-    -- install win32yank
-    vim.notify("Installing win32yank.exe")
-    if vim.fn.executable("curl") == 0 or vim.fn.executable("unzip") == 0 then
-      vim.notify("Failed to install win32yank:\nDon't have curl or unzip", vim.log.levels.WARN)
-      return
-    end
-    local url = "https://github.com/equalsraf/win32yank/releases/latest/download/win32yank-%s.zip"
-    if jit.arch == "x86" or jit.arch == "x64" then
-      url = string.format(url, jit.arch)
-    else
-      vim.notify("Just support x86 and x64.")
-      return
-    end
-
-    local tmp_file = os.tmpname() .. ".zip"
-    -- download win32yank
-    local out = vim.fn.system({ "curl", "-L", "-o", tmp_file, url })
-
-    if vim.v.shell_error ~= 0 then
-      vim.notify(string.format("Failed to download win32yank.exe:\n'%s'", out), vim.log.levels.WARN)
-      return
-    end
-
-    out = vim.fn.system({ "unzip", "-o", tmp_file, "-d", win32yank_dir })
-    if vim.v.shell_error ~= 0 then
-      vim.notify(string.format("Failed to extract files:\n'%s'", out), vim.log.levels.WARN)
-      os.remove(tmp_file)
-      return
-    end
-
-    os.remove(tmp_file)
-    out = vim.fn.system({ "chmod", "+x", win32yank_dir .. "/win32yank.exe" })
-    if vim.v.shell_error ~= 0 then
-      vim.notify(string.format("Failed to add execution mode:\n'%s'", out), vim.log.levels.WARN)
-      return
-    end
-    vim.notify("Successed to install win32yank")
-  end
+  local paste_cmd = "/mnt/c/Windows/System32/WindowsPowerShell/v1.0/powershell.exe \
+    -NoLogo -NoProfile \
+    -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace(\"`r\", \"\"))"
 
   vim.g.clipboard = {
-    name = "win32yank-wsl",
+    name = "WslClipboard",
     copy = {
-      ["+"] = "win32yank.exe -i --crlf",
-      ["*"] = "win32yank.exe -i --crlf",
+      ["+"] = "/mnt/c/Windows/System32/clip.exe",
+      ["*"] = "/mnt/c/Windows/System32/clip.exe",
     },
     paste = {
-      ["+"] = "win32yank.exe -o --lf",
-      ["*"] = "win32yank.exe -o --lf",
+      ["+"] = paste_cmd,
+      ["*"] = paste_cmd,
     },
     cache_enabled = true,
   }
