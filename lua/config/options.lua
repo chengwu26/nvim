2e39d0211a1506fbe701f92aae7e561574eecaf2
kanggui <kg.he@outlook.com>
2025-09-30 21:39:26 +0800

    feat(option): add more useful option
    Important Option
    - (diffopt)specifies diff algorithm: histogram
    - (matchpairs)add matchpairs "<>"
    - (helplang)change default help documentation language to Chinese
    - (shortmess)simplify the "CTRL-G" message
    - (showmatch)better parenthesis input experience
    - (virtualedit)better Select mode experience
    - (guifont)set default GUI font
    - (sidescrolloff)set horizontal scrolloff
    - (foldlevel, foldlevelstart)fold more than 5 levels of code
    - (formatoptions)the "gq" behavior
    
    Module
    - add utils module

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -12,56 +12,54 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
 local opt = vim.opt
--- general
-opt.swapfile = false
-opt.undofile = true
+
+-- [[ General ]]
+opt.diffopt:append({ "algorithm:histogram" })
+opt.helplang = "cn" -- Use Chinese documentation, but it doesn't work now.
+opt.jumpoptions = "stack"
+opt.matchpairs:append("<:>")
 opt.mouse = ""
-opt.jumpoptions = "stack" -- Make jumplist more intuitive
-opt.timeoutlen = 300
-opt.updatetime = 250
-opt.title = true
-opt.splitright = true
+opt.shortmess = "aoOCF"
+opt.showmatch = true
 opt.splitbelow = true
-
--- display
-opt.termguicolors = true
-opt.smoothscroll = true
-opt.showmode = false -- Use statusline plugin
-opt.winborder = "single"
-opt.cursorline = true
-
-opt.wrap = false
-opt.breakindent = true
-opt.signcolumn = "auto"
-opt.scrolloff = 5
-
-opt.number = true
-opt.relativenumber = true
-
--- tab & indent
-opt.expandtab = true
-opt.tabstop = 2
-opt.shiftwidth = 2
-opt.smartindent = true
-
-opt.list = true
-opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
-
+opt.splitright = true
+opt.swapfile = false
+opt.timeoutlen = 300
+opt.title = true
+opt.undofile = true
+opt.updatetime = 250
+opt.virtualedit = "block"
 -- search
 opt.ignorecase = true
 opt.smartcase = true
 
--- completion
-opt.completeopt = { "menu", "fuzzy", "noselect", "popup", "preview" }
+-- [[ Appearance ]]
+opt.conceallevel = 2
+opt.cursorline = true
+opt.guifont = "Maple Mono NF CN:h12"
+opt.signcolumn = "yes:1"
+opt.termguicolors = true
+opt.winborder = "single"
+-- scroll
+opt.scrolloff = 4
+opt.sidescrolloff = 8
+opt.smoothscroll = true
+-- warp
+opt.breakindent = true
+opt.linebreak = true
+opt.wrap = false
+-- line number
+opt.number = true
+opt.relativenumber = true
+-- tab & indent
+opt.expandtab = true
+opt.list = true
+opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
+opt.shiftwidth = 2
+opt.tabstop = 2
 
+-- [[ Code ]]
 -- fold
-opt.foldenable = true
-opt.foldlevel = 99
-opt.foldlevelstart = 99
-opt.foldmethod = "expr"
-opt.foldcolumn = "1"
-opt.fillchars = { eob = " ", fold = " ", foldopen = "", foldsep = " ", foldclose = "" }
-
 -- [[ Custom Fold-Text]]
 -- Source: https://www.reddit.com/r/neovim/comments/1fzn1zt/custom_fold_text_function_with_treesitter_syntax/
 local function fold_virt_text(result, start_text, lnum)
@@ -118,7 +116,15 @@ function _G.custom_foldtext()
   return result
 end
 vim.opt.foldtext = "v:lua.custom_foldtext()"
-
+opt.fillchars = { eob = " ", fold = " ", foldopen = "", foldsep = " ", foldclose = "" }
+opt.foldcolumn = "1"
+opt.foldlevel = 5
+opt.foldlevelstart = 5
+opt.foldmethod = "expr"
+-- completion
+opt.completeopt = { "menuone", "fuzzy", "noinsert", "popup" }
+-- formatting
+opt.formatoptions = "croqj"
 -- diagnostic
 vim.diagnostic.config({
   virtual_lines = { current_line = true },
@@ -134,55 +140,11 @@ vim.diagnostic.config({
 })
 
 -- [[ WSL Clipboard Integrate ]]
--- This solution use 'win32yank' to integrate system clipboard.
--- If not install win32yank, the code below will install it into `win32yank_dir`.
--- NOTE: (DEPENDENCE)
---  curl, unzip: install win32yank if it not installed (just for WSL)
-
-if os.getenv("WSL_DISTRO_NAME") ~= nil then
-  -- Build win32yank's install directory
-  local env_path = vim.env.PATH
-  local win32yank_dir = vim.fn.stdpath("data") .. "/win32yank"
-  vim.env.PATH = win32yank_dir .. ":" .. env_path
-
-  if vim.fn.executable("win32yank.exe") == 0 then
-    -- Install win32yank
-    vim.notify("Installing win32yank.exe")
-    if vim.fn.executable("curl") == 0 or vim.fn.executable("unzip") == 0 then
-      vim.notify("Failed to install win32yank:\nDon't have curl or unzip", vim.log.levels.WARN)
-      return
-    end
-    local url = "https://github.com/equalsraf/win32yank/releases/latest/download/win32yank-%s.zip"
-    if jit.arch == "x86" or jit.arch == "x64" then
-      url = string.format(url, jit.arch)
-    else
-      vim.notify("Just support x86 and x64.")
-      return
-    end
-
-    local tmp_file = os.tmpname() .. ".zip"
-    -- Download win32yank
-    local out = vim.fn.system({ "curl", "-L", "-o", tmp_file, url })
-
-    if vim.v.shell_error ~= 0 then
-      vim.notify(string.format("Failed to download win32yank.exe:\n'%s'", out), vim.log.levels.WARN)
-      return
-    end
-
-    out = vim.fn.system({ "unzip", "-o", tmp_file, "-d", win32yank_dir })
-    if vim.v.shell_error ~= 0 then
-      vim.notify(string.format("Failed to extract files:\n'%s'", out), vim.log.levels.WARN)
-      os.remove(tmp_file)
-      return
-    end
-
-    os.remove(tmp_file)
-    out = vim.fn.system({ "chmod", "+x", win32yank_dir .. "/win32yank.exe" })
-    if vim.v.shell_error ~= 0 then
-      vim.notify(string.format("Failed to add execution mode:\n'%s'", out), vim.log.levels.WARN)
-      return
-    end
-    vim.notify("Successed to install win32yank")
+-- This solution use `win32yank` to integrate system clipboard.
+local utils = require("modules.utils")
+if utils.is_wsl() then
+  if utils.setup_win32yank() then
+    return
   end
 
   vim.g.clipboard = {
