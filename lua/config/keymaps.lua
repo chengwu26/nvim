edc5a4f1aaebd0a8ce6c22a1c3f828fb734fa313
kanggui <kg.he@outlook.com>
2025-10-01 11:11:02 +0800

    refactor: refactor keymaps.lua

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,6 +1,6 @@
 ---@brief
 ---
---- Basic keymap
+--- Default keymap
 ---
 --- If you want to see plugin-related keymaps, they are in the
 --- plugin's own configuration, go to `lua/plugins/` and look for them.
@@ -10,62 +10,44 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
 local map = vim.keymap.set
--- basic
+
 map("i", "jk", "<ESC>", { silent = true })
-map(
-  "n",
-  "j",
-  "v:count == 0 ? 'gj' : 'j'",
-  { expr = true, silent = true, desc = "Move cursor down" }
-)
-map(
-  "x",
-  "j",
-  "v:count == 0 ? 'gj' : 'j'",
-  { expr = true, silent = true, desc = "Move cursor down" }
-)
-map("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true, desc = "Move cursor up" })
-map("x", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true, desc = "Move cursor up" })
 map("n", "<ESC>", "<CMD>nohlsearch<CR>")
+map("x", "/", "<ESC>/\\%V", { desc = "Search within Visual selection" })
+-- More consistent behavior of j/k when warp is set
+map({ "n", "x" }, "j", "v:count ? 'j' : 'gj'", { expr = true, silent = true })
+map({ "n", "x" }, "k", "v:count ? 'k' : 'gk'", { expr = true, silent = true })
+
 -- Quick replace string
 map("n", "<C-s>", ":%s/")
-map("v", "<C-s>", ":s/")
+map("x", "<C-s>", ":s/")
 
--- window
----@param target_direction 'h'|'j'|'k'|'l'
-local function smart_navigation(target_direction)
-  local current_wid = vim.api.nvim_get_current_win()
-  vim.cmd("wincmd " .. target_direction)
-  local new_wid = vim.api.nvim_get_current_win()
-
-  -- Specified direction haven't window and `neovim` inside `tmux` now.
-  -- Attempt focus `tmux` pane which specified direction, if any.
-  if (current_wid == new_wid) and (vim.env.TMUX ~= nil) then
-    local directions = { h = "L", j = "D", k = "U", l = "R" }
-    vim.fn.system({ "tmux", "select-pane", "-" .. directions[target_direction] })
-  end
-end
+-- Window operation
 map("n", "\\", "<CMD>sp<CR>", { desc = "Split window horizontally" })
 map("n", "|", "<CMD>vsp<CR>", { desc = "Split window vertically" })
-map("n", "<C-h>", function() smart_navigation("h") end, { desc = "Move focus to the left window" })
-map("n", "<C-l>", function() smart_navigation("l") end, { desc = "Move focus to the right window" })
-map("n", "<C-j>", function() smart_navigation("j") end, { desc = "Move focus to the lower window" })
-map("n", "<C-k>", function() smart_navigation("k") end, { desc = "Move focus to the upper window" })
+local smart_navigation = require("modules.utils").smart_navigation
+-- stylua: ignore start
+map({ "n", "t" }, "<C-h>", function() smart_navigation("h") end, { desc = "Focus the left window/pane" })
+map({ "n", "t" }, "<C-l>", function() smart_navigation("l") end, { desc = "Focus the right window/pane" })
+map({ "n", "t" }, "<C-j>", function() smart_navigation("j") end, { desc = "Focus the lower window/pane" })
+map({ "n", "t" }, "<C-k>", function() smart_navigation("k") end, { desc = "Focus the upper window/pane" })
+-- stylua: ignore end
 
--- yank & put
-map("v", "<leader>y", "\"+y", { desc = "Yank to clipboard" })
+-- Yank/Put to/from clipboard
+map("v", "<leader>y", "\"+y", { desc = "Yank selection to clipboard" })
 map("n", "<leader>y", "\"+yy", { desc = "Yank line to clipboard" })
 map("n", "<leader>Y", "\"+y$", { desc = "Yank to EOL to clipboard" })
 map("n", "<leader>p", "\"+p", { desc = "Put from clipboard" })
 
 -- options
 map("n", "<leader>tw", "<CMD>set invwrap<CR>", { desc = "Toggle Wrap" })
-map("n", "<leader>tn", "<CMD>set invrelativenumber<CR>", { desc = "Toggle Relative Number" })
+map("n", "<leader>tn", "<CMD>set invrnu<CR>", { desc = "Toggle Relative Number" })
 
--- Remove global LSP keymaps
-vim.keymap.del("n", "grn")
-vim.keymap.del({ "n", "v" }, "gra")
-vim.keymap.del("n", "grr")
-vim.keymap.del("n", "gri")
-vim.keymap.del("n", "gO")
-vim.keymap.del("i", "<C-s>")
+-- Remove default global LSP keymaps
+pcall(vim.keymap.del, "n", "grn")
+pcall(vim.keymap.del, { "n", "v" }, "gra")
+pcall(vim.keymap.del, "n", "grr")
+pcall(vim.keymap.del, "n", "grt")
+pcall(vim.keymap.del, "n", "gri")
+pcall(vim.keymap.del, "n", "gO")
+pcall(vim.keymap.del, "i", "<C-s>")
--- lua/modules/utils.lua
+++ lua/modules/utils.lua
@@ -4,6 +4,21 @@ local M = {}
 ---@return boolean
 M.is_wsl = function() return os.getenv("WSL_DISTRO_NAME") ~= nil end
 
+--- Seamless navigation with `tmux pane`
+---@param direction 'h'|'j'|'k'|'l'
+M.smart_navigation = function(direction)
+  local current_wid = vim.api.nvim_get_current_win()
+  vim.cmd("wincmd " .. direction)
+  local new_wid = vim.api.nvim_get_current_win()
+
+  -- Specified direction haven't window and `neovim` inside `tmux` now.
+  -- Attempt focus `tmux` pane which specified direction, if any.
+  if (current_wid == new_wid) and vim.env.TMUX then
+    local directions = { h = "-L", j = "-D", k = "-U", l = "-R" }
+    vim.fn.system({ "tmux", "select-pane", directions[direction] })
+  end
+end
+
 local function check_environment()
   if jit.arch ~= "x86" or jit.arch ~= "x64" then
     vim.notify("Only support the x86 and x64 platform.", vim.log.levels.WARN)
