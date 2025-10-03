309eceb01d30aa65cd38030c750db85aa4322b70
kanggui <kg.he@outlook.com>
2025-10-03 13:02:34 +0800

    feat(utils): add nvim-treesitter-textobjects

--- lua/plugins/utils/treesitter.lua
+++ lua/plugins/utils/treesitter.lua
@@ -1,25 +1,67 @@
 --- Tree-sitter configurations and abstraction layer.
 ---
 --- NOTE: (DEPENDENCE)
---- See also https://github.com/nvim-treesitter/nvim-treesitter?tab=readme-ov-file#requirements
+--- See also https://github.com/nvim-treesitter/nvim-treesitter/tree/main?tab=readme-ov-file#requirements
 ---
 
----@type LazySpec
+---@type LazySpec[]
 return {
-  "nvim-treesitter/nvim-treesitter",
-  lazy = false, -- This plugin does not support lazy-loading.
-  build = ":TSUpdate",
-  branch = "main",
-  config = function()
-    require("nvim-treesitter").install(CODE_CONF_FT):wait(300000)
+  {
+    "nvim-treesitter/nvim-treesitter",
+    lazy = false, -- This plugin does not support lazy-loading.
+    build = ":TSUpdate",
+    branch = "main",
+    config = function()
+      require("nvim-treesitter").install(CODE_CONF_FT):wait(300000)
 
-    vim.api.nvim_create_autocmd("FileType", {
-      pattern = CODE_CONF_FT,
-      callback = function()
-        vim.treesitter.start()
-        vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
-        vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
-      end,
-    })
-  end,
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = CODE_CONF_FT,
+        callback = function()
+          vim.treesitter.start()
+          vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+          vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+        end,
+      })
+    end,
+  },
+  {
+    "nvim-treesitter/nvim-treesitter-textobjects",
+    branch = "main",
+    ft = CODE_CONF_FT,
+    ---@type TSTextObjects.UserConfig
+    opts = {},
+    config = function(_, opts)
+      require("nvim-treesitter-textobjects").setup(opts)
+
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = CODE_CONF_FT,
+        callback = function(ev)
+          local map = vim.keymap.set
+          local ts_select = require("nvim-treesitter-textobjects.select").select_textobject
+          local mode = { "x", "o" }
+          --stylua: ignore start
+          map(mode, "af", function()
+            ts_select("@function.outer", "textobjects")
+          end, { buffer = ev.buf, desc = "A function" })
+
+          map(mode, "if", function()
+            ts_select("@function.inner", "textobjects")
+          end, { buffer = ev.buf, desc = "Inner function" })
+
+          map(mode, "ac", function()
+            ts_select("@class.outer", "textobjects")
+          end, { buffer = ev.buf, desc = "A class" })
+
+          map(mode, "ic", function()
+            ts_select("@class.inner", "textobjects")
+          end, { buffer = ev.buf, desc = "Inner class" })
+
+          map(mode, "as", function()
+            ts_select("@local.scope", "locals")
+          end, { buffer = ev.buf, desc = "A scope" })
+          --stylua: ignore end
+        end,
+      })
+    end,
+  },
 }
