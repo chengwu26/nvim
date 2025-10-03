1f1650b92463deb7d0d05c8fbd750f9d8b6735da
kanggui <kg.he@outlook.com>
2025-10-03 16:13:44 +0800

    fix(core): 'smart navigation' have unexpected behavior in float window

--- lua/modules/utils.lua
+++ lua/modules/utils.lua
@@ -17,13 +17,16 @@ do
   --- Seamless navigation with `tmux pane`
   ---@param direction 'h'|'j'|'k'|'l'
   M.smart_navigation = function(direction)
-    local current_wid = vim.api.nvim_get_current_win()
-    vim.cmd("wincmd " .. direction)
+    local curr_wid = vim.api.nvim_get_current_win()
+    local curr_win_conf = vim.api.nvim_win_get_config(curr_wid)
+    if curr_win_conf.relative == "" then
+      vim.cmd("wincmd " .. direction)
+    end
     local new_wid = vim.api.nvim_get_current_win()
 
     -- Specified direction haven't window and `neovim` inside `tmux` now.
     -- Attempt focus `tmux` pane which specified direction, if any.
-    if (current_wid == new_wid) and vim.env.TMUX then
+    if (curr_wid == new_wid) and vim.env.TMUX then
       local directions = { h = "-L", j = "-D", k = "-U", l = "-R" }
       vim.fn.system({ "tmux", "select-pane", directions[direction] })
     end
