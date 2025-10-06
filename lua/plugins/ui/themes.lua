cb4b8e38670d54212b613d397c8de9425acfa2be
kanggui <kg.he@outlook.com>
2025-10-06 10:24:27 +0800

    feat(ui): remove Themery plugin
    I don't have the habit of frequently switching themes

--- lua/plugins/ui/themes.lua
+++ lua/plugins/ui/themes.lua
@@ -1,32 +1,12 @@
----@brief
----
---- This module maintains all of themes/colorscheme .
----
---- You can use `:Themery` to switch theme (select: j, k; quit: q, <ESC>).
---- Since it isn't frequent to swithch theme, so there isn't keymap to switch
---- themes.
----
---- If you want to add another colorscheme, refer the comment of 'dependencies'
---- and 'opts' keys. Remove colorscheme also just need to delete it from
---- 'dependencies' and 'opts'.
-
 ---@type LazySpec
 return {
-  "zaldih/themery.nvim",
+  "catppuccin/nvim",
+  name = "catppuccin",
   lazy = false,
   priority = 1000,
-  opts = {
-    -- Add colorscheme's name at here to let 'Themery' to manage it.
-    themes = {
-      "default",
-      "catppuccin-latte",
-      "catppuccin-frappe",
-      "catppuccin-macchiato",
-      "catppuccin-mocha",
-    },
-  },
-  -- Add colorscheme as dependence to install and load them
-  dependencies = {
-    { "catppuccin/nvim", name = "catppuccin" },
-  },
+  opts = {},
+  config = function(_, opts)
+    require("catppuccin").setup(opts)
+    vim.cmd.colorscheme "catppuccin-mocha"
+  end,
 }
