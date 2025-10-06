751399c75114ae97aa5d4a717e198a2b6ef697c1
kanggui <kg.he@outlook.com>
2025-10-06 23:23:18 +0800

    chore: better code

--- lua/plugins/dev/mason.lua
+++ lua/plugins/dev/mason.lua
@@ -5,11 +5,9 @@ return {
   "mason-org/mason.nvim",
   cmd = "Mason",
   init = function()
-    if jit.os == "Windows" then
-      vim.env.PATH = vim.env.PATH .. ";" .. vim.fn.stdpath("data") .. "/mason/bin"
-    else
-      vim.env.PATH = vim.env.PATH .. ":" .. vim.fn.stdpath("data") .. "/mason/bin"
-    end
+    local bin_dir = vim.fs.joinpath(vim.fn.stdpath("data"), "mason", "bin")
+    local sep = jit.os == "Linux" and ":" or ";"
+    vim.env.PATH = vim.env.PATH .. sep .. bin_dir
   end,
   ---@class MasonSettings
   opts = { PATH = "skip" },
