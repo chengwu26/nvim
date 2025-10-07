ffcb2b380655af171644a96b590ba68c9c851117
kanggui <kg.he@outlook.com>
2025-10-07 19:21:18 +0800

    feat(dev): disable blink.cmd auto-insert when select item

--- lua/plugins/dev/completion.lua
+++ lua/plugins/dev/completion.lua
@@ -3,36 +3,25 @@ return {
   "saghen/blink.cmp",
   event = "LspAttach",
   version = "1.*",
-  dependencies = {
-    -- Optional: provides snippets for the snippet source
-    {
-      "rafamadriz/friendly-snippets",
-    },
-  },
-  ---@module 'blink.cmp'
+  dependencies = { "rafamadriz/friendly-snippets" },
+  init = function()
+    -- Disable native LSP completion
+    require("utils").del_matching_group("kg%.lsp%.complete")
+  end,
   ---@type blink.cmp.Config
   opts = {
-    -- See :h blink-cmp-config-keymap for defining your own keymap
     keymap = {
       preset = "super-tab", -- accept completion
-      -- Scroll documentation
       ["<C-u>"] = { "scroll_documentation_up", "fallback" },
       ["<C-d>"] = { "scroll_documentation_down", "fallback" },
     },
     fuzzy = { implementation = "prefer_rust_with_warning" },
     signature = { enabled = true },
     completion = {
-      documentation = {
-        auto_show = true,
-      },
-    },
-    sources = {
-      default = { "lsp", "path", "snippets", "buffer" },
+      documentation = { auto_show = true },
+      keyword = { range = "prefix" },
+      list = { selection = { auto_insert = false } },
     },
     cmdline = { enabled = false },
   },
-  init = function()
-    -- Disable native LSP completion
-    require("utils").del_matching_group("kg%.lsp%.complete")
-  end,
 }
