07432c2d6f043a48c1eed83d901f15e1c16d2e91
kanggui <kg.he@outlook.com>
2025-10-09 18:26:20 +0800

    perf(ui): explicit disable the catppucin integration
    This can speed up load times by around 0.5-0.8 milliseconds.

--- lua/plugins/ui/themes.lua
+++ lua/plugins/ui/themes.lua
@@ -4,7 +4,28 @@ return {
   name = "catppuccin",
   lazy = false,
   priority = 1000,
-  opts = {},
+  opts = {
+    integrations = {
+      which_key = true,
+
+      alpha = true,
+      cmp = false,
+      dap = false,
+      dap_ui = false,
+      dashboard = false,
+      fzf = false,
+      illuminate = { enabled = false },
+      indent_blankline = { enabled = true },
+      mini = false,
+      neogit = false,
+      neotree = false,
+      nvimtree = false,
+      rainbow_delimiters = false,
+      render_markdown = false,
+      treesitter_context = false,
+      ufo = false,
+    },
+  },
   config = function(_, opts)
     require("catppuccin").setup(opts)
     vim.cmd.colorscheme "catppuccin-mocha"
