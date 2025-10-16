fae0d21cc54c27da8ed2651d19e5f083f3f647a3
kanggui <kg.he@outlook.com>
2025-10-16 11:13:10 +0800

    chore(utils): remove redundant code

--- lua/utils/var.lua
+++ lua/utils/var.lua
@@ -6,7 +6,4 @@ local M = {}
 ---@type Env
 M.env = os.getenv("WSL_DISTRO_NAME") and "WSL" or jit.os
 
---- Wether or not is development environment
-M.is_dev = ENABLE_DEV or os.getenv("DEV_ENV") ---@diagnostic disable-line
-
 return M
